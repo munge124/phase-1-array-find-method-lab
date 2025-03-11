2b5a2c6487bbc7f14899db5fb8c19fd561e6edb2
benson <bensonmunge545@gmail.com>
2025-03-11 14:56:46 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W"); // Use find to locate a win
+    return win ? win.year : undefined; // If win is found, return the year, else return undefined
+  }
+  
